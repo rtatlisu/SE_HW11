f86157a9f1d2c811b313ce1a7113506621b5f6eb
Robin Tatlisu <robin.tatlisu@web.de>
2022-01-09 17:52:54 +0100

    changes on main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("master change");
+        System.out.println("changes on same position as Branch_1 from master branch");
     }
 }
