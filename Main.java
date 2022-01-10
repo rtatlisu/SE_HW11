58cea8416854ae2a80b4bee124112cdf67ba0598
Robin Tatlisu <robin.tatlisu@web.de>
2022-01-10 09:24:47 +0100

    :

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("master change");
-        System.out.println("changes on same position as Branch_1 from master branch");
-    }
-}
